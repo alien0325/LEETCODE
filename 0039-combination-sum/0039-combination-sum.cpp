7d179f127f8dec952685af5dccc492c6a8d9cc70
ANJAN DAS <anjand23@iitk.ac.in>
2026-08-17 01:41:24 +0530

    Time: 51 ms (10.52%), Space: 45.2 MB (10.63%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void Csum(int indx,int target,vector<int> &arr,vector<vector<int>> &ans,vector<int>ds){
+        if(indx == arr.size()){
+            if(target == 0){
+                ans.push_back(ds);
+            }
+            return;
+        }
+        if(arr[indx]<=target){
+            ds.push_back(arr[indx]);
+            Csum(indx,target-arr[indx],arr,ans,ds);
+            ds.pop_back();
+        }
+        Csum(indx+1,target,arr,ans,ds);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int> ds;
+        Csum(0,target,candidates,ans,ds);
+        return ans;
+    }
+};
