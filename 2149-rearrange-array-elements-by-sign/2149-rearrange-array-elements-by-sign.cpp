bc93657d25e6387a96515d824b4b9bfc285a7f9f
ANJAN DAS <anjand23@iitk.ac.in>
2026-08-17 02:21:34 +0530

    Time: 0 ms (100%), Space: 128.4 MB (33.62%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int len = nums.size();
+        vector<int> ans(len,0);
+        int pos_index = 0; int neg_index = 1;
+        for(int i = 0;i<len;i++){
+            if(nums[i]<0){
+                ans[neg_index] = nums[i];
+                neg_index+=2;
+            }
+            else{
+                ans[pos_index] = nums[i];
+                pos_index+=2;
+            }
+        }
+        return ans;
+    }
+};
