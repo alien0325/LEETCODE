b4757c5d935834332dbaa7ca20476a1184b90dc4
ANJAN DAS <anjand23@iitk.ac.in>
2026-09-08 00:22:07 +0530

    Time: 1 ms (45.94%), Space: 19.9 MB (46.94%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,39 +1,75 @@
+// class Solution {
+// public:
+//     void BFS(int u,unordered_map<int,vector<int>> &adj, vector<int> &vis){
+//         queue<int> q;
+//         q.push(u);
+//         vis[u] =1;
+//         while(!q.empty()){
+//             int node = q.front();
+//             q.pop();
+//             for(auto it: adj[node]){
+//                 if(!vis[it]){
+//                     vis[it]=1;
+//                     q.push(it);
+//                 }
+//             }
+//         }
+//     }
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         int n = isConnected.size();
+//         unordered_map<int,vector<int>> adj;
+//         for(int i = 0;i<n;i++){
+//             for(int j = 0;j<n;j++){
+//                 if(isConnected[i][j]==1){
+//                     adj[i].push_back(j);
+//                 }
+//             }
+//         }
+//         vector<int> vis(n,0);
+//         int provinces = 0;
+//         for(int i=0;i<n;i++){
+//             if(!vis[i]){
+//                 BFS(i,adj,vis);
+//                 provinces++;
+//             }
+//         }
+        
+//         return provinces;
+//     }
+// };
 class Solution {
 public:
-    void BFS(int u,unordered_map<int,vector<int>> &adj, vector<int> &vis){
+    void BFS(int u, vector<vector<int>>& isConnected, vector<int>& vis) {
         queue<int> q;
         q.push(u);
-        vis[u] =1;
-        while(!q.empty()){
+        vis[u] = 1;
+
+        while (!q.empty()) {
             int node = q.front();
             q.pop();
-            for(auto it: adj[node]){
-                if(!vis[it]){
-                    vis[it]=1;
-                    q.push(it);
+
+            for (int v = 0; v < isConnected.size(); v++) {
+                if (isConnected[node][v] == 1 && !vis[v]) {
+                    vis[v] = 1;
+                    q.push(v);
                 }
             }
         }
     }
+
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        unordered_map<int,vector<int>> adj;
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<n;j++){
-                if(isConnected[i][j]==1){
-                    adj[i].push_back(j);
-                }
-            }
-        }
-        vector<int> vis(n,0);
+
+        vector<int> vis(n, 0);
         int provinces = 0;
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                BFS(i,adj,vis);
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                BFS(i, isConnected, vis);
                 provinces++;
             }
         }
-        
+
         return provinces;
     }
 };
